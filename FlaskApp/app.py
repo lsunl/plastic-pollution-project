80b6cdf3d2881cfce4fbf5c11f4dc87f9690577c
arubalcaba001 <45408763+arubalcaba001@users.noreply.github.com>
2019-05-22 20:26:47 -0700

    updated

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,6 +1,5 @@
-# PLASTIC PLANET
-# 1. Import Flask
-# Python SQL toolkit and Object Relational Mapper
+# PLASTIC PLANET 
+# Import Flask
 import os
 import pandas as pd
 from flask import (
@@ -10,29 +9,33 @@ from flask import (
     request,
     redirect)
 
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-
-# 2. Create an app
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
+#################################################
+# Database Setup
+#################################################
 
-# 3. Database setup
+# Create our session (link) from Python to the DB
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("sqlite:///db/plasticWaste.sqlite")
 db = SQLAlchemy(app)
 
-# Reflect existing database into a new model
+# Reflect Database into ORM classes
 Base = automap_base()
-# Reflect the tables
-Base.prepare(db.engine, reflect=True)
+Base.prepare(engine, reflect=True)
 
-# Save references
+# Save references to each table
 country_geocode=Base.classes.country_geocode
-country_plastic_waste=Base.classes.country_plastic
+country_plastic_waste=Base.classes.country_plastic_waste
 country_waste_info=Base.classes.country_waste_info
 master=Base.classes.master
 mismanaged_plastic_pperson=Base.classes.mismanaged_plastic_pperson
@@ -43,14 +46,11 @@ waste_types=Base.classes.pperson_plastic_waste
 
 
 
-# 4. Define static routes
+# 3. Define static routes
 @app.route("/")
 def index():
     """ landing page """
     return render_template("index.html")
-
-
-
 
 @app.route("/api")
 def api_list():
@@ -58,34 +58,21 @@ def api_list():
     print (
         f"Plastic Planet Navigation<br/>"
         f"Available Routes:<br/>"
-        f"/us-plastic-lifecycle<br/>"
+        f"/global-waste<br/>"
         f"/population-vs-waste-management<br/>"
         f"/countries-mismatched-waste"
     )
+    
+@app.route("/api/global_waste")
+def global_waste():
+
+
+
 
 @app.route("/api/epa_data")
 def epa_data():
-    results = db.session.query(plasticWaste.id, plasticWaste.waste_type).all()
-    return jsonify(results)
-
-
-
-
-# @app.route("/api/us-plastic-lifecycle")
-# def lifecycle():
-#     return jsonify()
-#
-# @app.route("/api/population-vs-waste-management")
-# def stations():
-#     return jsonify()
-#
-#
-# @app.route("/api/countries-mismatched-waste")
-# def tobs():
-#
-#     return jsonify(obs_dict)
-
-
+    epa_results = db.session.query(plasticWaste.id, plasticWaste.waste_type).all()
+    return jsonify(epa_results)
 
 # 4. Define main behavior
 if __name__ == "__main__":
