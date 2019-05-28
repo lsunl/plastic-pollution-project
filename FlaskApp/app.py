1abc180ae76699f72b7836c95da4914d9f122972
Laura Sun <sun.laura@outlook.com>
2019-05-28 12:42:09 -0700

    alright now

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -21,28 +21,35 @@ from sqlalchemy import create_engine, func
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///resources/plastichorror.sqlite", echo=False)
+
+# DB Version 1 - Separate tables
+# Trouble with joining tables
+# sqlite:///resources/database.sqlite
+
+#DB Version 2 - One master table
+# sqlite:///resources/database2.sqlite
+
+engine = create_engine("sqlite:///resources/database2.sqlite", echo=False)
 conn = engine.connect()
-
-
 
 # Reflect Database into ORM classes
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
+# DB Version 1 references
+# epa = Base.classes.epa
+# global_waste = Base.classes.global_waste
+# trades = Base.classes.trades
+# treatment_types = Base.classes.treatment_types
+# countries = Base.classes.countries
 
-# Reference to EPA route
-epa_waste = Base.classes.epa_waste
-# country_geocode=Base.classes.country_geocode
+# DB Version 2 references
+countries = Base.classes.countries
+epa = Base.classes.epa
 
-# Reference to GLOBAL WASTE route
-country_plastic_waste=Base.classes.country_plastic_waste
-# country_waste_info=Base.classes.country_waste_info
-# plastic_waste_percentage=Base.classes.plastic_waste_percentage
-# pperson_plastic_waste=Base.classes.pperson_plastic_waste
 
-# Reference to PREDICTIONS route
-mismanaged_prediction=Base.classes.mismanaged_prediction
+# DB Version 2 references
+# tables = Base.classes.master
 
 session = Session(bind=engine)
 
@@ -61,56 +68,66 @@ def index():
     return render_template("index.html")
 
 
+
 @app.route("/api/epa-waste")
 def example():
-    results = session.query(epa_waste).all()
+    results = session.query(epa).all()
     #unravel_string = list(np.ravel(results))
     all = []
     for x in results:
         x_dict = {}
         # x_dict["epa_id"] = x.epa_id
         x_dict["year"] = x.year
-        x_dict["paper_paperboard"] = x.paper_paperboard
-        x_dict["glass"] = x.glass
-        x_dict["metals"] = x.metals
-        x_dict["plastic"] = x.plastic
-        x_dict["wood"] = x.wood
-        x_dict["type_id"] = x.type_id
-        x_dict["waste_type"] = x.waste_type
+        x_dict["tons"] = x.values
+        x_dict["waste_material"] = x.waste_material
         all.append(x_dict)
-
     return jsonify(all)
 
-@app.route("/api/global-waste")
-def global_waste():
-    results = session.query(country_plastic_waste).all()
+
+@app.route("/api/countries")
+def info():
+    # results = session.query(countries, global_waste).join(global_waste, countries.country_id == global_waste.country_id).filter(global_waste.year == "2025").all()
+    results = session.query(countries).all()
     all = []
     for x in results:
         x_dict = {}
-        x_dict["country_id"] = x.country_id
-        x_dict["Country"] = x.Country
-        x_dict["Plastic_waste_generation_kg_day"] = x.Plastic_waste_generation_kg_day
-        x_dict["Managed_plastic_waste_kg_day"] = x.Managed_plastic_waste_kg_day
-        x_dict["Mismanaged_plastic_waste_kg_day"] = x.Mismanaged_plastic_waste_kg_day
+        # x_dict["country_id"] = x.country_id
+        x_dict["country"] = x.country_x
+        x_dict["longitude"] = x.longitude
+        x_dict["latitude"] = x.latitude
+        x_dict["values"] = x.value
         all.append(x_dict)
-
     return jsonify(all)
 
 @app.route("/api/projections")
-def projections():
-    results = session.query(mismanaged_prediction).all()
+def predictions():
+    # results = session.query(countries, global_waste).join(global_waste, countries.country_id == global_waste.country_id).filter(global_waste.year == "2025").all()
+    results = session.query(countries).all()
     all = []
     for x in results:
         x_dict = {}
-        x_dict["country_id"] = x.country_id
-        x_dict["Country"] = x.Country
-        x_dict["Mismanaged_plastic_waste_in_2010_tonnes"] = x.Mismanaged_plastic_waste_in_2010_tonnes
-        x_dict["Mismanaged_plastic_waste_in_2025_tonnes"] = x.Mismanaged_plastic_waste_in_2025_tonnes
-        x_dict["Per_Change_2010_to_2025"] = x.Per_Change_2010_to_2025
+        # x_dict["country_id"] = x.country_id
+        x_dict["country"] = x.country_x
+        x_dict["status"] = x.status
+        x_dict["metric"] = x.metric
+        x_dict["values"] = x.value
         all.append(x_dict)
-
     return jsonify(all)
 
+# @app.route("/api/global-waste")
+# def global_waste():
+#     results = session.query(country_plastic_waste).all()
+#     all = []
+#     for x in results:
+#         x_dict = {}
+#         x_dict["country_id"] = x.country_id
+#         x_dict["Country"] = x.Country
+#         x_dict["Plastic_waste_generation_kg_day"] = x.Plastic_waste_generation_kg_day
+#         x_dict["Managed_plastic_waste_kg_day"] = x.Managed_plastic_waste_kg_day
+#         x_dict["Mismanaged_plastic_waste_kg_day"] = x.Mismanaged_plastic_waste_kg_day
+#         all.append(x_dict)
+#
+#     return jsonify(all)
 
 
 
